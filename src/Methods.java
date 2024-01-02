58301a82c55fc03644821cc5695005f1a2a0d03e
Sayyed Muhammad Jaffry <muhammadj110@gmail.com>
2024-01-03 01:16:50 +0330

    changed sortBy() method to sort based on any array, not just allProcesses.

--- src/Methods.java
+++ src/Methods.java
@@ -2,23 +2,24 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Methods {
-    public static void sortBy(String type) { // n=0: arrival, n=1: priority, n=2 burst time
-        int n=-1;
+    public static void sortBy(String type, ArrayList<Process> array) { // n=0: arrival, n=1: priority, n=2 burst time
+        int n = -1;
         switch (type) {
             case "arrival" -> n = 0;
             case "priority" -> n = 1;
             case "remaining" -> n = 2;
         }
-        for (int i = 0; i < Process.allProcesses.size(); i++)
-            for (int j = 0; j < Process.allProcesses.size() - i - 1; j++)
-                if (Process.allProcesses.get(j).attrs[n] > Process.allProcesses.get(j + 1).attrs[n]) {
+        for (int i = 0; i < array.size(); i++)
+            for (int j = 0; j < array.size() - i - 1; j++)
+                if (array.get(j).attrs[n] > array.get(j + 1).attrs[n]) {
                     //swap a[j] and a[j+1]
-                    Process temp = Process.allProcesses.get(j);
-                    Process.allProcesses.set(j, Process.allProcesses.get(j + 1));
-                    Process.allProcesses.set(j + 1, temp);
+                    Process temp = array.get(j);
+                    array.set(j, array.get(j + 1));
+                    array.set(j + 1, temp);
                 }
     }
 
